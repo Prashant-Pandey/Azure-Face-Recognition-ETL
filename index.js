8de16bd168c722ae812426ee3a065a6265345ed9
Prashant Pandey <prashant.pandey.dev@gmail.com>
2020-12-18 21:53:21 -0500

    added postman and documentation redirect

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 var app = express();
 const v1 = require("./routes/");
 
-app.use('/uploads', express.static('./uploads/'));
+app.use('/postman', express.static('./postman/'));
 app.use(express.json())
 app.use('/v1', v1);
 
-
+app.use('/', (req, res)=>{
+  res.redirect("http://35.184.178.64:4000/v1/docs");
+})
 
 app.listen(4000, () => console.log('now graphql at localhost:4000'));
